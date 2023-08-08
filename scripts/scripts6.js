1de615aae6c3b1ef1c664191266e9379ed76a134
brayam.rendon <brayam.rendon10@gmail.com>
2023-08-08 15:07:36 -0500

    Solucion punto 6 y 7

--- scripts/scripts6.js
+++ scripts/scripts6.js
@@ -1,8 +1,11 @@
 /*6- Un maestro desea saber el porcentaje de hombres y de mujeres que hay en
 un grupo de estudiantes.*/
 
-let quantityGroup=parseFloat(prompt("Ingrese la cantidad total de estudiantes")),
+let 
     quantityMan=parseFloat(prompt("Ingrese la cantidad de Hombres")),
     quantityWomen=parseFloat(prompt("Ingrese la cantidad de Mujeres")),
-    porcentageMen=();
+    quantityGroup=(quantityMan+quantityWomen),
+    porcentageMen=((quantityMan*100)/quantityGroup),
+    porcentageWomen=(100-porcentageMen);
 
+alert("El porcentaje de hombres es "+porcentageMen+" % y el porcentaje de mujeres es "+porcentageWomen+" %");
---
+++ scripts/scripts7.js
@@ -0,0 +1,9 @@
+/*7- Escriba un algoritmo para saber si un número es par o impar.*/
+
+let number=parseFloat(prompt("Ingrese un numero"));
+
+if (number %2==0){
+    alert("El "+number+" es par")
+}else{
+    alert("El "+number+" es impar")
+}
